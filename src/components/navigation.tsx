84ddc6e29de9852506b99d1a861e146bdef4caaa
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2023-07-29 10:43:48 -0400

    adj menu  toggling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,37 +1,35 @@
+import React, { useState } from 'react';
 import { Nav, Navbar } from "react-bootstrap";
 
 const Navigation: React.FC = () => {
-  const toggleActive = (): void => {
-    const hamburger = document.querySelector<HTMLElement>(".ham-icon");
-    const navigation = document.querySelector<HTMLElement>(".nav");
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-    if (hamburger && navigation) {
-      hamburger.classList.toggle("active");
-      navigation.classList.toggle("active");
-    }
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState);
   };
 
   return (
-    <div>
-      <Navbar className="navigation" fixed="top" expand="sm">
+    <Navbar className="navigation" fixed="top" expand="sm">
+        <>
         <div className="branding">
-          <div><a href="#home" className="company-logo"><img src="/public/agavivasq.ico" className="gavipic" /></a></div>
-          <div><a href="#home" className="company-name">Agavivasq</a></div>
+            <div><a href="#home" className="company-logo"><img src="/public/agavivasq.ico" className="gavipic" alt="Company Logo" /></a></div>
+            <div><a href="#home" className="company-name">Agavivasq</a></div>
         </div>
-        <Nav className="nav">
-          <Nav.Link href="#about" className="nav-link">About</Nav.Link>
-          <Nav.Link href="#art" className="nav-link">Art Projects</Nav.Link>
-          <Nav.Link href="#webapps" className="nav-link">Web Projects</Nav.Link>
-          <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+        <Nav className={`nav ${isMenuOpen ? 'active' : ''}`}>
+            <Nav.Link href="#about" className="nav-link" key="about">About</Nav.Link>
+            <Nav.Link href="#art" className="nav-link" key="art">Art Projects</Nav.Link>
+            <Nav.Link href="#webapps" className="nav-link" key="webapps">Web Projects</Nav.Link>
+            <Nav.Link href="#contact" className="nav-link" key="contact">Contact</Nav.Link>
         </Nav>
-        <div className="ham-icon" onClick={toggleActive}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
+        <div className={`ham-icon ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
         </div>
-      </Navbar>
-    </div>
+        </>
+    </Navbar>
   );
 };
 
 export default Navigation;
+
