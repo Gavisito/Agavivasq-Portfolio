30c24e21574a1104d68bcf210cb4ee8955dc3eee
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2023-07-29 11:40:10 -0400

    added scroll animation and blue link underlining

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Nav, Navbar } from "react-bootstrap";
+import { Link } from 'react-scroll';
 
 const Navigation: React.FC = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -8,28 +9,38 @@ const Navigation: React.FC = () => {
     setMenuOpen((prevState) => !prevState);
   };
 
+  const handleNavLinkClick = () => {
+    // Close the menu on mobile after clicking a link
+    setMenuOpen(false);
+  };
+
   return (
     <Navbar className="navigation" fixed="top" expand="sm">
-        <>
+      <>
         <div className="branding">
-            <div><a href="#home" className="company-logo"><img src="/public/agavivasq.ico" className="gavipic" alt="Company Logo" /></a></div>
-            <div><a href="#home" className="company-name">Agavivasq</a></div>
+          <div>
+            <a href="#home" className="company-logo">
+              <img src="/agavivasq.ico" className={`gavipic ${isMenuOpen ? 'active' : ''}`} alt="Company Logo" />
+            </a>
+          </div>
+          <div>
+            <a href="#home" className={`company-name ${isMenuOpen ? 'active' : ''}`}>Agavivasq</a>
+          </div>
         </div>
         <Nav className={`nav ${isMenuOpen ? 'active' : ''}`}>
-            <Nav.Link href="#about" className="nav-link" key="about">About</Nav.Link>
-            <Nav.Link href="#art" className="nav-link" key="art">Art Projects</Nav.Link>
-            <Nav.Link href="#webapps" className="nav-link" key="webapps">Web Projects</Nav.Link>
-            <Nav.Link href="#contact" className="nav-link" key="contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to="about" smooth={true} offset={-100} className="nav-link"  onClick={handleNavLinkClick}>About</Nav.Link>
+          <Nav.Link as={Link} to="art" smooth={true} offset={-100} className="nav-link" onClick={handleNavLinkClick}>Art Projects</Nav.Link>
+          <Nav.Link as={Link} to="webapps" smooth={true} offset={-100} className="nav-link" onClick={handleNavLinkClick}>Web Projects</Nav.Link>
+          <Nav.Link as={Link} to="contact" smooth={true} offset={-100} className="nav-link" onClick={handleNavLinkClick}>Contact</Nav.Link>
         </Nav>
         <div className={`ham-icon ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
         </div>
-        </>
+      </>
     </Navbar>
   );
 };
 
 export default Navigation;
-
