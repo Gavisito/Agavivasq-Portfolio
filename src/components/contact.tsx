983458bc7c79753d1ac31f33c792cbe48a4b1512
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2023-08-07 15:24:32 -0400

    added labels to inputs

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -5,15 +5,18 @@ const Contact: React.FC = () => {
         <h2>Contact Me</h2>
         <form>
           <div className="fm-row-one">
-            <div>
+            <div className="inputs_section">
+              <label htmlFor="from_name">Name</label>
               <input type="text" name="from_name" placeholder="Enter Your Name" required />
             </div>
-            <div>
+            <div className="inputs_section">
+              <label htmlFor="from_email">Email</label>
               <input type="email" name="from_email" placeholder="Enter Your Email" required />
             </div>
           </div>
           <div className="fm-row-two">
-            <div>
+          <div className="inputs_section">
+              <label htmlFor="message">Message</label>
               <textarea name="message" placeholder="Write your message about any particular inquiries you have" required />
             </div>
           </div>
