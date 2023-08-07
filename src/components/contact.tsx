03415cf005841319ca50d247cfc73dc86f6e8a79
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2023-08-07 16:00:07 -0400

    adj form and overall layout

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -6,17 +6,17 @@ const Contact: React.FC = () => {
         <form>
           <div className="fm-row-one">
             <div className="inputs_section">
-              <label htmlFor="from_name">Name</label>
+              <label htmlFor="from_name">Name:</label>
               <input type="text" name="from_name" placeholder="Enter Your Name" required />
             </div>
             <div className="inputs_section">
-              <label htmlFor="from_email">Email</label>
+              <label htmlFor="from_email">Email:</label>
               <input type="email" name="from_email" placeholder="Enter Your Email" required />
             </div>
           </div>
           <div className="fm-row-two">
           <div className="inputs_section">
-              <label htmlFor="message">Message</label>
+              <label htmlFor="message">Message:</label>
               <textarea name="message" placeholder="Write your message about any particular inquiries you have" required />
             </div>
           </div>
