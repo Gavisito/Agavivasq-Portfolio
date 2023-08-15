30830ebe7beb240c817b83442fa2a8c911e33690
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2023-08-15 16:37:43 -0400

    add leaflet map and contact info links

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,3 +1,6 @@
+import { MapContainer, TileLayer, Marker} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 const Contact: React.FC = () => {
   return (
     <div id="contact" className="contact">
@@ -17,13 +20,31 @@ const Contact: React.FC = () => {
           <div className="fm-row-two">
           <div className="inputs_section">
               <label htmlFor="message">Message:</label>
-              <textarea name="message" placeholder="Write your message about any particular inquiries you have" required />
+              <textarea name="message" placeholder="Write your inquires, comments, and concerns here. Thank you!" required />
             </div>
           </div>
           <div>
             <input type="submit" name="submitbtn" value="Send Message" required />
           </div>
         </form>
+      </div>
+      <div className="leafletMap">
+        <MapContainer center={[38.8315, -77.3117]} zoom={13} >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[38.8315, -77.3117]}/>
+          </MapContainer>
+          <div className="mapInfo">
+              <h3>Location & Contacts</h3>
+              <h5 style={{marginBottom: '5%'}}>Save them for later!</h5>
+              <p>Phone: <a href="tel:5716429492">(571)-642-9492</a></p>
+              <p>Email: <a href="mailto:agavivasq@gmail.com">agavivasq@gmail.com</a></p>
+          </div>
+
+
       </div>
     </div>
   );
